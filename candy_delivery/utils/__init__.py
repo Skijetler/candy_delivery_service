b68637e7cbda597b6d3880cfc41f408d95af6680
anon <anon@mail.net>
2021-03-22 23:55:15 +0300

    fix utils module + fix deploy

---
+++ candy_delivery/utils/__init__.py
@@ -0,0 +1,18 @@
+"""
+Сервис доставки конфет с REST API
+"""
+__author__ = 'Игорь Стикентзин'
+__maintainer__ = __author__
+
+__email__ = 'i.stikentzin@yandex.ru'
+__license__ = 'MIT'
+__version__ = '1.0'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
